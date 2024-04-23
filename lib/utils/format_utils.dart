c43972c00604c1e9892fa8160b468c07676a50ef
AustinPardosi <gabrielpardosi26@gmail.com>
2024-04-23 19:28:12 +0700

    fix: adding validator & date converter class, connecting add-meal to be

---
+++ lib/utils/format_utils.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class FormatUtils {
+  static String? formatToIsoDate(String? dateStr) {
+    if (dateStr == null) return null;
+    try {
+      DateTime date = DateFormat('dd-MM-yyyy').parseLoose(dateStr);
+      return DateFormat("yyyy-MM-dd'T'00:00:00Z").format(date) + 'Z';
+    } catch (error) {
+      return null;
+    }
+  }
+
+  static String? formatToIsoDateTime(DateTime? dateTime) {
+    return dateTime?.toIso8601String();
+  }
+
+  static String? combineDateWithTime(DateTime? date, TimeOfDay? time) {
+    if (date == null || time == null) return null;
+    DateTime combinedDateTime =
+        DateTime(date.year, date.month, date.day, time.hour, time.minute);
+    return DateFormat("yyyy-MM-dd'T'HH:mm:ss").format(combinedDateTime) + 'Z';
+  }
+}
