3cfe673b31cefbf0233ec6aa307455b8a43c52cb
Margaretha Olivia <margarethaolivia41@gmail.com>
2024-04-26 15:28:58 +0700

    feat: nutrition detail page

---
+++ lib/pages/scan-food-detail.dart
@@ -0,0 +1,99 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+class ScanFoodDetailPage extends StatelessWidget {
+  const ScanFoodDetailPage({Key? key, required this.picture}) : super(key: key);
+
+  final XFile picture;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset('assets/topbar-logo.png'),
+        centerTitle: true,
+        toolbarHeight: 60.0,
+        elevation: 20,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _buildSquareImage(),
+            const SizedBox(height: 16),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Text(
+                'Spaghetti',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            _buildNutritionTable(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSquareImage() {
+    return Container(
+      width: 300,
+      height: 350,
+      child: Image.file(File(picture.path), fit: BoxFit.cover),
+    );
+  }
+
+  Widget _buildNutritionTable(BuildContext context) {
+    return Card(
+      elevation: 4,
+      margin: const EdgeInsets.all(18),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'Nutrition Details',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 6),
+            _buildNutritionRow('Calories', '300', 'cal'),
+            _buildNutritionRow('Carbs', '20', 'gr'),
+            _buildNutritionRow('Protein', '10', 'gr'),
+            _buildNutritionRow('Fat', '13', 'gr'),
+            _buildNutritionRow('Fiber', '2', 'gr'),
+            _buildNutritionRow('Glucose', '2', 'gr'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNutritionRow(String label, String value, String unit) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center, // Aligns items along the Y-axis (vertically)
+        children: [
+          Text(
+              label,
+              style: const TextStyle(
+                  fontSize: 18)
+          ),
+          Text(
+            value + ' ' + unit,
+            style: const TextStyle(
+                fontSize: 18),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/scan-food.dart
+++ lib/pages/scan-food.dart
@@ -1,7 +1,7 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'preview-camera.dart';
+import 'scan-food-detail.dart';
 
 class ScanFoodPage extends StatefulWidget {
   const ScanFoodPage({Key? key, required this.cameras}) : super(key: key);
@@ -41,7 +41,7 @@ class _ScanFoodPageState extends State<ScanFoodPage> {
       Navigator.push(
           context,
           MaterialPageRoute(
-              builder: (context) => PreviewPage(
+              builder: (context) => ScanFoodDetailPage(
                 picture: picture,
               )
           )
@@ -68,52 +68,109 @@ class _ScanFoodPageState extends State<ScanFoodPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-          child: Stack(children: [
-            (_cameraController.value.isInitialized)
-                ? CameraPreview(_cameraController)
-                : Container(
-                color: Colors.black,
-                child: const Center(child: CircularProgressIndicator())),
-            Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  height: MediaQuery.of(context).size.height * 0.20,
-                  decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
-                      color: Colors.black),
-                  child:
-                  Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
-                    Expanded(
-                        child: IconButton(
-                          padding: EdgeInsets.zero,
-                          iconSize: 30,
-                          icon: Icon(
-                              _isRearCameraSelected
-                                  ? CupertinoIcons.switch_camera
-                                  : CupertinoIcons.switch_camera_solid,
-                              color: Colors.white),
-                          onPressed: () {
-                            setState(
-                                    () => _isRearCameraSelected = !_isRearCameraSelected);
-                            initCamera(widget.cameras![_isRearCameraSelected ? 0 : 1]);
-                          },
-                        )),
-                    Expanded(
-                        child: IconButton(
-                          onPressed: takePicture,
-                          iconSize: 50,
-                          padding: EdgeInsets.zero,
-                          constraints: const BoxConstraints(),
-                          icon: const Icon(Icons.circle, color: Colors.white),
-                        )
-                    ),
-                    const Spacer(),
-                  ]),
-                )
+      appBar: AppBar(
+        title: Image.asset('assets/topbar-logo.png'),
+        centerTitle: true,
+        toolbarHeight: 60.0,
+        elevation: 20,
+      ),
+      body: Stack(
+        children: [
+          _cameraPreviewWidget(),
+          _buildDescription(),
+          _buildBottomButtons(),
+        ],
+      ),
+    );
+  }
+
+  Widget _cameraPreviewWidget() {
+    return Positioned.fill(
+      child: _cameraController.value.isInitialized
+          ? CameraPreview(_cameraController)
+          : Container(
+        color: Colors.black,
+        child: const Center(child: CircularProgressIndicator()),
+      ),
+    );
+  }
+
+  Widget _buildDescription() {
+    return Positioned(
+      top: 0,
+      left: 0,
+      right: 0,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+        color: Colors.black.withOpacity(0.4),
+        child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Scan Food',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
             ),
-          ]),
-        )
+            SizedBox(height: 8),
+            Text(
+              'Take a photo of your food and we\'ll give you its nutritional information',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBottomButtons() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.12,
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
+          color: Colors.black,
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: IconButton(
+                padding: EdgeInsets.zero,
+                iconSize: 30,
+                icon: Icon(
+                  _isRearCameraSelected
+                      ? CupertinoIcons.switch_camera
+                      : CupertinoIcons.switch_camera_solid,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _isRearCameraSelected = !_isRearCameraSelected;
+                  });
+                  initCamera(widget.cameras![_isRearCameraSelected ? 0 : 1]);
+                },
+              ),
+            ),
+            Expanded(
+              child: IconButton(
+                onPressed: takePicture,
+                iconSize: 50,
+                padding: EdgeInsets.zero,
+                constraints: const BoxConstraints(),
+                icon: const Icon(Icons.circle, color: Colors.white),
+              ),
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
     );
   }
 }
