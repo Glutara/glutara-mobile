1d50ee24958db28510dc07c3c2007d7f92c09ac3
AustinPardosi <gabrielpardosi26@gmail.com>
2024-05-06 09:56:05 +0700

    fix: handling empty notification

--- lib/pages/notification.dart
+++ lib/pages/notification.dart
@@ -1,35 +1,6 @@
 import 'package:flutter/material.dart';
 
-final notifications = [
-  {
-    'title': 'Sleep Reminder',
-    'time': '22.00',
-    'description':
-        "It's time to unwind and get a good night's sleep for your well-being.",
-    'isNew': true
-  },
-  {
-    'title': 'Medication Reminder',
-    'time': '11.30',
-    'description':
-        "It's time to take 500g of Metformin to support your health and well-being.",
-    'isNew': true
-  },
-  {
-    'title': 'Time to Workout',
-    'time': '10.00',
-    'description':
-        'Engage in your scheduled exercise routine for a healthier you.',
-    'isNew': true
-  },
-  {
-    'title': 'Medication Reminder',
-    'time': '20.00',
-    'description':
-        "It's time to take 500g of Metformin to support your health and well-being.",
-    'isNew': false
-  },
-];
+final notifications = [];
 
 class NotificationPage extends StatelessWidget {
   const NotificationPage({Key? key});
@@ -48,48 +19,62 @@ class NotificationPage extends StatelessWidget {
             centerTitle: false,
             elevation: 0,
           ),
-          body: ListView.builder(
-            itemCount: notifications.length,
-            itemBuilder: (context, index) {
-              final notification = notifications[index];
-              return Card(
-                margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                child: ListTile(
-                  title: Text(
-                    notification['title'] as String,
-                    style: const TextStyle(fontSize: 18),
+          body: notifications.isEmpty
+              ? Center(
+                  child: Text(
+                    'No notification to display',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey,
+                    ),
                   ),
-                  subtitle: Text(
-                    notification['description'] as String,
-                    style: const TextStyle(fontSize: 13)),
-                  trailing: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      if (notification['isNew'] as bool)
-                        Text(
-                          notification['time'] as String,
-                          style: TextStyle(
-                              color: Theme.of(context).colorScheme.onSurfaceVariant,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 14),
-                        )
-                      else
-                        Text(
-                          'Yesterday',
-                          style: TextStyle(
-                            color: Theme.of(context).colorScheme.onSurfaceVariant,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 14,
-                          ),
+                )
+              : ListView.builder(
+                  itemCount: notifications.length,
+                  itemBuilder: (context, index) {
+                    final notification = notifications[index];
+                    return Card(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 4),
+                      child: ListTile(
+                        title: Text(
+                          notification['title'] as String,
+                          style: const TextStyle(fontSize: 18),
                         ),
-                    ],
-                  ),
-                  isThreeLine: true,
+                        subtitle: Text(notification['description'] as String,
+                            style: const TextStyle(fontSize: 13)),
+                        trailing: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            if (notification['isNew'] as bool)
+                              Text(
+                                notification['time'] as String,
+                                style: TextStyle(
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .onSurfaceVariant,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 14),
+                              )
+                            else
+                              Text(
+                                'Yesterday',
+                                style: TextStyle(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .onSurfaceVariant,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 14,
+                                ),
+                              ),
+                          ],
+                        ),
+                        isThreeLine: true,
+                      ),
+                    );
+                  },
                 ),
-              );
-            },
-          ),
         ),
       ),
     );
